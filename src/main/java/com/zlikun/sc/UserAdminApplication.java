476ef9f2f8126f06fcfe2c43117eff9ee02858d3
zlikun <zlikun-dev@hotmail.com>
2018-07-26 16:34:23 +0800

    配合测试eureka集群部分节点关闭后，REST服务可以正常使用

--- src/main/java/com/zlikun/sc/UserAdminApplication.java
+++ src/main/java/com/zlikun/sc/UserAdminApplication.java
@@ -9,7 +9,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  * http://cloud.spring.io/spring-cloud-static/Finchley.RELEASE/multi/multi__spring_cloud_commons_common_abstractions.html#_serviceregistry
  */
 // 启用服务发现客户端配置
-// autoRegister = true 为默认值，通常不需要配置，
+// autoRegister = true 为默认值，通常不需要配置，等价：eureka.client.register-with-eureka: true
 // 除非不注册到注册中心(本工程不对外提供服务，原则上可以不注册)
 @EnableDiscoveryClient
 @SpringBootApplication
